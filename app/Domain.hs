a35007bb2f86ebe6393dbbe6f05d265749543175
Rex Ng <rex.ng@digitalasset.com>
2023-05-22 14:08:08 +0800

    Init

---
+++ app/Domain.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLists, ViewPatterns, PatternSynonyms #-}
+
+
+module Domain where
+
+import Data.ByteString.Lazy.UTF8
+import Data.Char
+import Data.Set
+import Text.Read (readEither)
+
+infixr 5 :<
+
+pattern (:<) :: Char -> ByteString -> ByteString
+pattern b :< bs <- (uncons -> Just (b, bs))
+
+pattern Empty :: ByteString
+pattern Empty   <- (uncons -> Nothing)
+
+data Colour = White | Black deriving (Eq, Show)
+
+data Move =
+    Move MoveInfo
+  | Capture MoveInfo
+  | Check MoveInfo
+  | Checkmate MoveInfo
+  | Stalemate MoveInfo
+
+data MoveInfo = MoveInfo
+  { piece :: Piece 
+  , colour :: Colour
+  , position :: Position }
+
+type ParserError = String
+
+newtype ChessMoveParser a = ChessMoveParser 
+  { runParser :: ByteString -> Either ParserError (ByteString, a) }
+
+parseRank :: Char -> ChessMoveParser Char
+parseRank c = ChessMoveParser $ \input ->
+  let ranks = ['a'..'h'] :: Set Char
+  in case input of
+    x :< xs | x == c && c `member` ranks -> Right (xs, c)
+    [] -> Left "Invalid rank"
+
+parseFile :: Int -> ChessMoveParser Int
+parseFile c = ChessMoveParser $ \input ->
+  let files = [1..8] :: Set Int
+  in case input of
+    x :< xs | x == c && c `member` files -> Right (xs, c)
+    [] -> Left "Invalid file"
+
+parsePosition :: ChessMoveParser Position
+parsePosition = ChessMoveParser $ readEither . fmap toLower . toString
+
+data Piece =
+    Pawn
+  | Knight
+  | Bishop
+  | Rook
+  | Queen
+  | King
+  deriving Eq
+
+instance Show Piece where
+  show Pawn = ""
+  show Knight = "N"
+  show Bishop = "B"
+  show Rook = "R"
+  show Queen = "Q"
+  show King = "K"
+
+data Position =
+   A8 | B8 | C8 | D8 | E8 | F8 | G8 | H8 |
+   A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7 |
+   A6 | B6 | C6 | D6 | E6 | F6 | G6 | H6 |
+   A5 | B5 | C5 | D5 | E5 | F5 | G5 | H5 |
+   A4 | B4 | C4 | D4 | E4 | F4 | G4 | H4 |
+   A3 | B3 | C3 | D3 | E3 | F3 | G3 | H3 |
+   A2 | B2 | C2 | D2 | E2 | F2 | G2 | H2 |
+   A1 | B1 | C1 | D1 | E1 | F1 | G1 | H1
+   deriving (Eq, Show, Read)
